648551c2b5ba973f0b4a5501f63bd1b018ce4e68
Bernardo Rosas <124811292+BernaRM111@users.noreply.github.com>
2023-06-05 20:47:26 -0600

    Update schema.py

--- futbolistas/schema.py
+++ futbolistas/schema.py
@@ -6,8 +6,6 @@ import users.schema
 class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
-
 class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
